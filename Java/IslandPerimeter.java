862d863c63b0fb31be327d9b0d15eabe4a1ed6f8
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-10-25 21:35:42 -0500

    Add files via upload

--- Java/IslandPerimeter.java
+++ Java/IslandPerimeter.java
@@ -1,11 +1,11 @@
 /*
 Kevin Nguyen
-A basic program where given a matrix, check if the diagonals are the same value
+A basic program where given a matrix, determine the perimeter of an island.
 
 How to run (linux):
 	1.) open command line and cd into directory folder of where this code is
 	2.) type in "javac *.java" to compile java files
-	3.) type in "java ToeplitzMatrix"
+	3.) type in "java IslandPerimeter"
 */
 
 //imports
@@ -13,7 +13,7 @@ import java.io.*;
 import java.util.*;
 
 //class
-class ToeplitzMatrix {
+class IslandPerimeter {
 	//Global Declarations
 
 	//main driver
@@ -22,46 +22,10 @@ class ToeplitzMatrix {
 		
 		//get matrix
 
-		//call isToeplitzMatrix
+		//call 
 
 		//result output
 	}
 
-    public boolean isToeplitzMatrix(int[][] matrix) {
-        //local declarations
-        int i = 0, j = 0, k = 0, v = 0, rowLimit = matrix.length, colLimit = 0, tgt = 0;
-        boolean good = true;
-        
-        //empty case
-        if(matrix.length < 1) {
-            return true;
-        }
-        
-        //iterate over rows
-        while(i < rowLimit && good) {
-            colLimit = matrix[i].length;
-            //iterate over cols
-            while(j < colLimit && good) {
-                k = i; v = j;
-                tgt = matrix[k][v];
-                //go diagonally
-                while(good) {
-                    //conditional to ensure bounds are maintained
-                    if(k >= rowLimit || v >= colLimit) {
-                        break;
-                    }
-                    //check diagonal
-                    if(matrix[k][v] != tgt) {
-                        good = false;
-                        break;
-                    }
-                    // System.out.println(matrix[k][v]);
-                    k++; v++;
-                }
-                j++;
-            }
-            i++; j=0;
-        }
-        return good;   
-    }
+
 }
