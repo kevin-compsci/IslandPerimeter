2f02b6d23f07c7479df9753ae5fa0512f07fb466
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-11-04 23:35:10 -0600

    Add files via upload

--- Java/IslandPerimeter.java
+++ Java/IslandPerimeter.java
@@ -22,10 +22,77 @@ class IslandPerimeter {
 		
 		//get matrix
 
-		//call 
+		//call islandPerimeter function
 
 		//result output
 	}
 
+    //IslandPerimeter will return the value (int) of the perimeter of the island given a matrix
+    public int islandPerimeter(int[][] grid) {
+        //local declarations
+        int i = 0, j = 0, rowLimit = grid.length, colLimit = 0, k = 0, v = 0, perim = 0;
+        
+        //empty case
+        if(grid.length < 1) {
+            return 0;
+        }
+        
+        //iteate over rows
+        while(i < rowLimit) {
+            colLimit = grid[i].length;
+            //iterate over cols
+            while(j < colLimit) {
+                k = i; v = j;
+                
+                if(grid[i][j] == 1) {
+                    //check left
+                    v--;
+                    if(v < 0) {
+                        perim++;
+                    }
+                    else {
+                        if(grid[k][v] == 0) {
+                            perim++;
+                        }
+                    }
 
+                    //check right
+                    v = j; v++;
+                    if(v >= colLimit) {
+                        perim++;
+                    }
+                    else {
+                        if(grid[k][v] == 0) {
+                            perim++;
+                        }
+                    }
+
+                    //check top
+                    v = j; k--;
+                    if(k < 0) {
+                        perim++;
+                    }
+                    else {
+                        if(grid[k][v] == 0) {
+                            perim++;
+                        }
+                    }
+
+                    //check bot
+                    k = i; k++;
+                    if(k >= rowLimit) {
+                        perim++;
+                    }
+                    else {
+                        if(grid[k][v] == 0) {
+                            perim++;
+                        }
+                    }
+                }
+                j++;
+            }
+            i++; j=0;
+        }
+        return perim;
+    }
 }
